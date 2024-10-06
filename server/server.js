d980e5f23ab9f93f27de7e4275a89013e7573f32
GitShailendra <kumarshailrndra7355@gmail.com>
2024-10-06 16:19:26 +0530

    code updated

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(cors());
 app.use(express.json());
@@ -53,7 +54,9 @@ app.post('/api/subscribe', async (req, res) => {
     }
   });
 
-
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
